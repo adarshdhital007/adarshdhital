379405ef7e33574781eb8856074be3b5c2dd35be
Adarsh Dhital Khatri <adarsh7466@gmail.com>
2023-09-14 01:36:55 +0530

    Updated Columns for Small Devices

--- src/components/home/EmojiBullet.js
+++ src/components/home/EmojiBullet.js
@@ -6,7 +6,7 @@ function EmojiBullet(props) {
 
     return (
         <Box component={'li'} fontSize={'1rem'} lineHeight={1.5} style={{cursor: 'default'}}>
-            <Box component={'span'} aria-label="cheese"
+            <Box component={'span'} 
                  role="img"
                  mr={{xs: '0.5rem', md: '1rem'}} fontSize={'1.5rem'}>{emoji}</Box> {text}
         </Box>
